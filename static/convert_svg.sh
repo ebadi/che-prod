7d74dc055f15aa9c9ccbed4030e4f6f1fd28a85d
Hamid Ebadi <hamid.ebadi@gmail.com>
2023-10-27 17:31:52 +0200

    update web

--- static/convert_svg.sh
+++ static/convert_svg.sh
@@ -14,13 +14,6 @@ convert -density 200 logo-with-circle.svg -resize 180x180 -transparent white log
 convert -density 200 logo-with-circle.svg -resize 350x350 -transparent white logo-with-circle350x350.png
 convert -density 200 logo-with-circle.svg -resize 512x512 -transparent white logo-with-circle512x512.png
 
-convert -density 200 logo-with-circle.svg -resize 180x180 -transparent white logo-with-circle180x180.png
-convert -density 200 logo-with-circle.svg -resize 57x57 -transparent white apple-touch-icon-57x57.png
-
-
-
-
-
 convert -density 256x256 -background transparent logo.svg -define icon:auto-resize -colors 256 favicon.ico
 rm ../favicon.ico
 cp favicon.ico ../favicon.ico
